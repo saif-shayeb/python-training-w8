8003da8ca95d818f6501d95f27f20036eaff6dc1
saif-shayeb <sshayep@gmail.com>
2026-04-08 21:55:03 +0300

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app.models.courses import Course
 from app.models.instructor import Instructor
@@ -12,6 +11,7 @@ from werkzeug.security import generate_password_hash
 from database import Base, db_session, engine
 from app import create_app
 import pytest
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 @pytest.fixture(scope="session")
